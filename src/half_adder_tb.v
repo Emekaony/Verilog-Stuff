2bd011939602ce31ff1113673567ed94229f306e
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2023-02-13 17:16:45 -0800

    Implemtnted the half adder with test bench working correctly

--- src/half_adder_tb.v
+++ src/half_adder_tb.v
@@ -1,3 +1,30 @@
 /*
 this is the test bench for the half adder
 */
+
+`include "/Users/shadon./Developer/verilog_stuff/src/half_adder.v"
+
+module half_adder_tb;
+    reg a, b;
+
+    // to declare output use wire
+    wire sum, carry;
+
+    half_adder add1(a, b, sum, carry);
+
+    initial begin
+        // dump the changes in of the code
+        // vcd means value changed dump
+        $dumpfile("half_adder.vcd");
+        $dumpvars(0, half_adder_tb);
+
+        a=0; b=0;
+        #1
+        a=0; b=1;
+        #1
+        a=1; b=0;
+        #1
+        a=1; b=1;
+    end
+
+endmodule
